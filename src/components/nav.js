37c518dab56e8f00c8ac1745f9b512c432c31e99
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-06-26 18:49:03 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
+
+var user = prompt('Enter 0000 for trading \n Enter Pin for Admin purpose')
+
 export default class Navbar extends Component {
     render() {
+        if(user=='1234')
+        {
         return (
-            <div>
+            <div >
                 <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
                     <Link to="/" className="navbar-brand">TEC LOGO</Link>
                     <div className="collpase navbar-collapse">
@@ -14,22 +19,10 @@ export default class Navbar extends Component {
                     <li className="navbar-item">
                     <Link to="/" className="nav-link">Home</Link>
                     </li>
+                  
 
                     <li className="navbar-item">
-                    <Link to="/about" className="nav-link">About</Link>
-                    </li>
-
-
-                    <li className="navbar-item">
-                    <Link to="/schedule" className="nav-link right">Schedule</Link>
-                    </li>
-
-                    <li className="navbar-item">
-                    <Link to="/contact" className="nav-link right">Contact</Link>
-                    </li>
-
-                    <li className="navbar-item">
-                    <Link to="/trade" className="nav-link right">StockTrade</Link>
+                    <Link to="/stock" className="nav-link right">StockTrade</Link>
                     </li>
 
                     </ul>
@@ -38,6 +31,26 @@ export default class Navbar extends Component {
                 </nav>
             </div>
         )
+        }
+        if(user=='0000')
+        {
+            return (
+                <div >
+                    <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
+                        <Link to="nil" className="navbar-brand">TEC LOGO</Link>
+                        <div className="collpase navbar-collapse">
+                        <ul className="navbar-nav ">
+                        <li className="navbar-item">
+                        <Link to="/stock" className="nav-link right"></Link>
+                        </li>
+                        <h4 className="text-white m-2 font-weight-bold">Welcome to Stock Trade</h4>
+                        </ul>
+                      
+                        </div>
+                    </nav>
+                </div>                
+            )
+        }
     }
 }
 
