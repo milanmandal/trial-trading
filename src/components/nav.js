1081e35bed829c2921103ed51a058997adcaff95
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-06-29 16:38:53 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,55 +2,32 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
 
-var user = prompt('Enter 0000 for trading \n Enter Pin for Admin purpose')
+//var user = prompt('Enter 0000 for trading \n Enter Pin for Admin purpose')
 
 export default class Navbar extends Component {
     render() {
-        if(user=='1234')
-        {
-        return (
-            <div >
-                <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
-                    <Link to="/" className="navbar-brand">TEC LOGO</Link>
-                    <div className="collpase navbar-collapse">
-                    
-                    <ul className="navbar-nav ">
-
-                    <li className="navbar-item">
-                    <Link to="/" className="nav-link">Home</Link>
-                    </li>
-                  
-
-                    <li className="navbar-item">
-                    <Link to="/stock" className="nav-link right">StockTrade</Link>
-                    </li>
-
-                    </ul>
-                  
-                    </div>
-                </nav>
-            </div>
-        )
-        }
-        if(user=='0000')
-        {
+      
+     
             return (
                 <div >
                     <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
-                        <Link to="nil" className="navbar-brand">TEC LOGO</Link>
+                        <Link to="nil" className="navbar-brand ">TEC CLUB</Link>
                         <div className="collpase navbar-collapse">
                         <ul className="navbar-nav ">
                         <li className="navbar-item">
-                        <Link to="/stock" className="nav-link right"></Link>
+                        <Link to="/" className="nav-link right">Stock Trade</Link>
                         </li>
-                        <h4 className="text-white m-2 font-weight-bold">Welcome to Stock Trade</h4>
+                        <li className="navbar-item">
+                        <Link to="/admin" className="nav-link right">Admin</Link>
+                        </li>
+                       
                         </ul>
                       
                         </div>
                     </nav>
                 </div>                
             )
-        }
+        
     }
 }
 
